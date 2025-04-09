dcf74a64c86349b06df31163f2576f25c8e6aaae
glo.pena <glo@glo.cloud>
2025-04-09 23:52:08 +0100

    read a file and output its lenght

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	fmt.Println("Words Counter")
+	content, error := os.ReadFile("words.txt")
+
+	if error != nil {
+		fmt.Println("Error:", error)
+		return
+	}
+	words := string(content)
+	wordsCount := len(words)
+	fmt.Println("Total words:", wordsCount)
 }
