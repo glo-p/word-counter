e2c54ed5c73d9222e837fba422a0248a413a0879
glo.pena <glo@glo.cloud>
2025-04-10 00:55:38 +0100

    refactored tests

--- main_test.go
+++ main_test.go
@@ -5,23 +5,25 @@ import (
 )
 
 type TestCase struct {
+	name     string
 	content  []byte
 	expected int
 }
 
 func TestCountWords(t *testing.T) {
 	testCases := []TestCase{
-		{[]byte("Hello world"), 2},
-		{[]byte("Hello   world"), 2},
-		{[]byte("   Hello world"), 2},
-		{[]byte("Hello world "), 2},
-		{[]byte("Hello"), 1},
-		{[]byte(""), 0},
+		{name: "Simple case", content: []byte("Hello world"), expected: 2},
+		{name: "Multiple spaces", content: []byte("Hello   world"), expected: 2},
+		{name: "Leading spaces", content: []byte("   Hello world"), expected: 2},
+		{name: "Trailing spaces", content: []byte("Hello world "), expected: 2},
+		{name: "Single word", content: []byte("Hello"), expected: 1},
+		{name: "Empty string", content: []byte(""), expected: 0},
 	}
 
 	for _, testCase := range testCases {
 		actual := countWords(testCase.content)
 		if actual != testCase.expected {
+			t.Error(testCase.name)
 			t.Errorf("Expected %d, but got %d", testCase.expected, actual)
 		}
 	}
